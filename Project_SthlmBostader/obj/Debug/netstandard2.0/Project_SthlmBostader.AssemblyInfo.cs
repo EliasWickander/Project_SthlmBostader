370f9398b4462cfa91bb1cb5c0fcb9ba6787bda9
EliasWickander <elias.wt@hotmail.com>
2024-06-28 18:02:16 +0200

    Configured Android/IOS build & dev settings

--- Project_SthlmBostader/obj/Debug/netstandard2.0/Project_SthlmBostader.AssemblyInfo.cs
+++ Project_SthlmBostader/obj/Debug/netstandard2.0/Project_SthlmBostader.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_SthlmBostader")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+760d38e41402ae995ce20c037c8f950b9a0a6787")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+315baceb54551cff4ee4d3154ebc5dba53372349")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_SthlmBostader")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_SthlmBostader")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
